4843c15659fe8249d967063e9453a25ad32f6466
Youngho <cyh6099@gmail.com>
2023-07-18 20:05:37 +0900

    feat: initial commit

---
+++ our-memory/src/main/java/keepsake/ourmemory/OurMemoryApplication.java
@@ -0,0 +1,13 @@
+package keepsake.ourmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurMemoryApplication.class, args);
+	}
+
+}
---
+++ our-memory/src/test/java/keepsake/ourmemory/OurMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package keepsake.ourmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
