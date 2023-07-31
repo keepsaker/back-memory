ca881284a5bd518535fcdb786edc467428c97900
Youngho <cyh6099@gmail.com>
2023-07-31 23:41:41 +0900

    feature: 추억 저장 api작성

---
+++ our-memory/src/main/java/keepsake/ourmemory/api/MemoryController.java
@@ -0,0 +1,24 @@
+package keepsake.ourmemory.api;
+
+import keepsake.ourmemory.application.MemoryCreateRequest;
+import keepsake.ourmemory.application.MemoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoryController {
+
+    private final MemoryService memoryService;
+
+    @PostMapping("/memories")
+    public ResponseEntity<Void> createMemory(@RequestBody MemoryCreateRequest request) {
+        Long memoryId = memoryService.createMemory(request);
+        return ResponseEntity.created(URI.create("/memories/" + memoryId)).build();
+    }
+}
---
+++ our-memory/src/test/java/keepsake/ourmemory/api/MemoryControllerTest.java
@@ -0,0 +1,53 @@
+package keepsake.ourmemory.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import keepsake.ourmemory.application.MemoryCreateRequest;
+import keepsake.ourmemory.application.MemoryService;
+import keepsake.ourmemory.domain.memory.Category;
+import keepsake.ourmemory.domain.memory.Star;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemoryController.class)
+class MemoryControllerTest {
+
+    @MockBean
+    private MemoryService memoryService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void 추억을_새긴다() throws Exception {
+        MemoryCreateRequest request = new MemoryCreateRequest("title", Category.CAFE.getCategoryName(), LocalDateTime.now(), Star.TWO.getValue(), "content", List.of("image"));
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        given(memoryService.createMemory(any()))
+                .willReturn(1L);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/memories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getHeader("location")).isEqualTo("/memories/1");
+    }
+}
