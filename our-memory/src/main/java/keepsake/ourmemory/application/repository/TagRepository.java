195f49c4896acbbd571aeff48d229d7533001a13
Youngho <cyh6099@gmail.com>
2023-08-10 14:44:07 +0900

    feat: 멤버 별 태그 조회 repository method구현 및 테스트

--- our-memory/src/main/java/keepsake/ourmemory/application/repository/TagRepository.java
+++ our-memory/src/main/java/keepsake/ourmemory/application/repository/TagRepository.java
@@ -3,5 +3,9 @@ package keepsake.ourmemory.application.repository;
 import keepsake.ourmemory.domain.tag.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    List<Tag> findTagsByMemberId(Long memberId);
 }
